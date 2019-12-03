b016963166d58a12a8d48e226f55da999e58762b
cam <1743982333@qq.com>
2019-12-03 17:34:36 +0800

    xxx

--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -8,3 +8,41 @@ function getScore() {
     let arr = ['4.7', '4.8', '4.9'];
     return arr[getRandomNumber(0, arr.length - 1)];
 }
+
+function toNormalNumber(str, int = true) {
+    if (!int) {
+        return Number(str.replace(',', ''));
+    }
+    return parseInt(str.replace(',', ''));
+}
+
+function getGrade() {
+    let grade = [{
+            g: 'LV1',
+            max: '99.1'
+        },
+        {
+            g: 'LV2',
+            max: '999.1'
+        },
+        {
+            g: 'LV3',
+            max: '9,999.1'
+        },
+        {
+            g: 'LV4',
+            max: '99,999.1'
+        }
+    ]
+    grade = grade.map((i) => {
+        console.log(toNormalNumber(i.max) / 10, toNormalNumber(i.max), getRandomNumber(toNormalNumber(i.max) / 10, toNormalNumber(i.max)));
+        let d = getRandomNumber(toNormalNumber(i.max) / 10, toNormalNumber(i.max));
+        return {
+            ...i,
+            d,
+            c: Number(toNormalNumber(i.max, false) - d).toLocaleString()
+        }
+    });
+    console.log(grade[getRandomNumber(0, grade.length - 1)]);
+    return grade[getRandomNumber(0, grade.length - 1)]
+}
