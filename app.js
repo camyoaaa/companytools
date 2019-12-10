7fbb94ceacc35b5f6533ba4fa980b83b21dbbdba
cam <1743982333@qq.com>
2019-12-10 10:09:46 +0800

    增加密码控制

--- app.js
+++ app.js
@@ -1,41 +1,73 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var router = express.Router();
+var path = require("path");
+const bodyParser = require("body-parser"); //请求体解析器
+var cookieParser = require("cookie-parser"); //cookie解析器
+var logger = require("morgan"); //日志模块,只能记录请求信息
+var session = require("express-session");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var auth = require("./auth");
 
 var app = express();
 
+// 使用 session 中间件
+app.use(
+    session({
+        secret: "secret", // 对session id 相关的cookie 进行签名
+        resave: true,
+        saveUninitialized: false, // 是否保存未初始化的会话
+        cookie: {
+            maxAge: 1000 * 60 * 3 // 设置 session 的有效时间，单位毫秒
+        }
+    })
+);
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+app.use(cookieParser()); //使用cookie解析中间件
+app.use(bodyParser.json()); //使用请求体解析中间件
+app.use(auth);
+app.use(express.static(path.join(__dirname, "public"))); //静态化public目录
+app.all("*", function(req, res, next) {
+    //设置跨域
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", " 3.2.1");
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,123 @@
 var express = require('express');
 var router = express.Router();
 
+const DB = require('../models');
+const UserModel = DB.getModel('User');
+const {
+  sendValidCode
+} = require('../tool/sendSMS');
+
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
-router.get('/login', function (req, res, next) {
-  console.log(req);
-  res.send('respond with a resource');
+router.get('/login', async function (req, res, next) {
+
+
+  const {
+    phone,
+    password,
+    validCode
+  } = req.body;
+  try {
+    let users = await UserModel.findOne({
+      phone,
+      password
+    });
+    if (validCode != users) {
+      res.json({
+        status: 400,
+        msg: '验证码错误'
+      });
+    }
+    if (users) {
+      res.cookie('userid', users._id); //登录时候设置cookie
+    }
+    res.json({
+      status: users ? 200 : 400,
+      msg: users ? '用户名或者密码错误' : '登陆成功'
+    });
+  } catch (error) {}
+
 });
 router.post('/regist', function (req, res, next) {
-  console.log(req);
-  res.send(req);
+  const {
+    phone,
+    password,
+    validcode
+  } = req.body;
+  UserModel.find({
+    phone
+  }, (err, doc) => {
+    if (!err) {
+      if (doc.length > 0) {
+        res.json({
+          status: 400,
+          msg: '该手机号码已被注册'
+        });
+      } else {
+        UserModel.create({
+          phone,
+          password,
+          validcode
+        }, (err, doc) => {
+          if (!err) {
+            res.json({
+              status: 200,
+              msg: '注册成功'
+            });
+          } else {
+            res.send(err);
+          }
+        });
+      }
+    }
+  });
+
+});
+router.post('/isRegist', async function (req, res, next) {
+  try {
+    const {
+      phone
+    } = req.body;
+    let queryResult = await UserModel.find({
+      phone
+    });
+    res.json({
+      status: 200,
+      isRegist: queryResult.length > 0
+    });
+  } catch (error) {}
+
+});
+router.post('/validcode', async function (req, res, next) {
+  const {
+    phone
+  } = req.body;
+  try {
+    const {
+      sixValidCode,
+      result
+    } = await sendValidCode(phone);
+    UserModel.update({
+      phone
+    }, {
+      $set: {
+        validcode: sixValidCode
+      }
+    });
+    res.json({
+      status: 200,
+      sixValidCode,
+      result
+    });
+  } catch (error) {
+    res.json({
+      status: 400,
+      msg: error.message
+    });
+  }
 });
 
 module.exports = router;
